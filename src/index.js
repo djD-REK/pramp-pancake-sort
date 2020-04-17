a648ccefaa61b4183e4d9c66a3d358bba6d7b9fd
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-04-17 12:23:50 -0400

    Ran prettier

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "./styles.css"
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
@@ -6,16 +6,16 @@ document.getElementById("app").innerHTML = `
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
-`;
+`
 
 // Modifies the array in place, ala Array.prototype.sort()
 const flip = (array, numberOfItems) => {
-  array.splice();
-};
+  array.splice()
+}
 // Sorts the array in place, ala Array.prototype.sort((a,b)=>a-b)
 const pancakeSort = array => {
   // your code goes here
-};
+}
 
 /* Pancake Sort
 Given an array of integers arr:
